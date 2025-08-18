1b4e74c80f6381b821bf5f53ef535693673d2789
Sergio Gevatschnaider <132850405+sgevatschnaider@users.noreply.github.com>
2025-08-18 14:02:18 -0300

    Create test_operators.py

---
+++ tests/test_operators.py
@@ -0,0 +1,14 @@
+import random
+from classroom.evo.operators import ox, swap_mutation
+
+def test_ox_is_permutation():
+    p1 = list(range(20)); p2 = p1[::-1]
+    c1, c2 = ox(p1, p2)
+    assert sorted(c1) == list(range(20))
+    assert sorted(c2) == list(range(20))
+
+def test_swap_mutation_changes_or_not():
+    random.seed(0)
+    p = list(range(10))
+    swap_mutation(p, 1.0)
+    assert p != list(range(10))
